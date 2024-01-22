468a83aca670fa8b93603e8e9a3ed296554085f7
mbgh2 <ddvolf@gmail.com>
2024-01-22 17:19:28 +0200

    js_03

--- js/script_03.js
+++ js/script_03.js
@@ -3,30 +3,30 @@ const elemOne = document.querySelector('.one');
 const elemTwo = document.querySelector('.two');
 
 
-// function test(color) {
-//    this.style.background = color;
-// }
-// let col = 'red';
-// let testBind = test.bind(elemTwo, col);
-// elemOne.onclick = testBind;
+function test(color) {
+   this.style.background = color;
+}
+let col = 'red';
+let testBind = test.bind(elemTwo, col);
+elemOne.onclick = testBind;
 
 
-// function hi(name) {
-//    console.log(`Hi, ${name}! I'm ${this.family}`);
-// }
-// const user = {
-//    family: 'Voldemar'
-// }
-// const user2 = {
-//    family: 'Dmitry'
-// }
-// let hi1 = hi.bind(user, 'Natasha');
-// let hi2 = hi.bind(user2, 'Natasha');
+function hi(name) {
+   console.log(`Hi, ${name}! I'm ${this.family}`);
+}
+const user = {
+   family: 'Voldemar'
+}
+const user2 = {
+   family: 'Dmitry'
+}
+let hi1 = hi.bind(user, 'Natasha');
+let hi2 = hi.bind(user2, 'Natasha');
 
-// hi1();
-// hi2();
+hi1();
+hi2();
 
-// hi.call(user, 'Tanya');
+hi.call(user, 'Tanya');
 // =====================================
 
 
